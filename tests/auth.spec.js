1eef1ac5d48d4a751e27cd2b2630d2e9e5482c7a
Eva Niehaus <vl1vl@yahoo.com>
2021-07-18 04:20:03 +0200

    add mocha hooks

--- tests/auth.spec.js
+++ tests/auth.spec.js
@@ -1,30 +1,41 @@
-import { expect } from 'chai';
+import {expect} from 'chai';
 import supertest from 'supertest';
 
-
 describe('Auth', function () {
+    let result;
 
-    const request = supertest(process.env.BASE_URL);
-
-
-    it('Successful log in', function() {
-        const a = process.env.BASE_URL;
-        request
+   describe('Successful log in', function () {
+        before(function(){
+        result = supertest(process.env.BASE_URL)
             .post('/auth')
-            .send({ login:process.env.LOGIN, password: process.env.PASSWORD })
-              .end(function (err, res) {
-                expect(res.statusCode).to.eq(200);
+            .send({login: process.env.LOGIN, password: process.env.PASSWORD});
+        })
+
+        it('Response status code is 200', function () {
+            result.expect(200);
+        });
+
+        it('Response body contains authorization token', function () {
+            result.end(function (err, res) {
                 expect(res.body.token).not.to.be.undefined;
             });
+        });
     });
 
-    it('Log in with wrong credentials should return error', function(){
-        request
-            .post('/auth')
-            .send( { login: 'wrong', password: 'wrong' })
-            .end(function(err, res) {
-                expect(res.statusCode).to.eq(404);
+    describe('Log in with wrong credentials should return error', function () {
+        before(function(){
+            result = supertest(process.env.BASE_URL)
+                .post('/auth')
+                .send({login: 'wrong', password: 'wrong'});
+        });
+        it('Response status code is 404', function () {
+            result.expect(404);
+        });
+
+        it('Response body contains error message', function () {
+            result.end(function (err, res) {
                 expect(res.body.message).to.eq('Wrong login or password.');
             });
+        });
     });
 });
