8ed6368a178521c4cb62456c285619def93297e4
EvaN <vl1vl@yahoo.com>
2021-08-03 22:26:26 +0200

    remove logging for global before hook update environment variable name in pipeline

--- config/setup.js
+++ config/setup.js
@@ -11,10 +11,7 @@ if(baseUrl.includes('localhost') && baseUrl.includes(port))
     start(port);
 
 before(async function () {
-      console.log(`BASE_URL: ${process.env.BASE_URL}`);
-      console.log(`LOGIN: ${process.env.LOGIN}`);
-      console.log(`PASSWORD: ${process.env.PASSWORD}`);
-      await supertest(process.env.BASE_URL)
+    await supertest(process.env.BASE_URL)
         .post('/auth')
         .send({login: process.env.LOGIN, password: process.env.PASSWORD})
         .then((res) => { process.env.TOKEN = res.body.token });
