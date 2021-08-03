0cb5aa1cf61592fdc969641cdc5c97ab23fcc0ad
Eva Niehaus <78087549+eva-nie@users.noreply.github.com>
2021-08-03 22:40:46 +0200

    add api tests pipeline
    * add api tests pipeline
    
    * add logging for global before hook
    
    * remove logging for global before hook
    update environment variable name in pipeline

--- config/setup.js
+++ config/setup.js
@@ -11,7 +11,7 @@ if(baseUrl.includes('localhost') && baseUrl.includes(port))
     start(port);
 
 before(async function () {
-      await supertest(process.env.BASE_URL)
+    await supertest(process.env.BASE_URL)
         .post('/auth')
         .send({login: process.env.LOGIN, password: process.env.PASSWORD})
         .then((res) => { process.env.TOKEN = res.body.token });
